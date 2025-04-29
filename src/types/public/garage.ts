61159f245d3a2b2f6f45dd9998a602be78204551
atamayev <bluedotrobots@gmail.com>
2025-04-29 13:23:49 -0400

    Message builder and protocol

--- src/types/public/garage.ts
+++ src/types/public/garage.ts
@@ -1,9 +1,25 @@
+import { PipUUID } from "./utils"
+
 export type LightAnimation =
-| "No animation"
-| "Breathing"
-| "Rainbow"
-| "Strobe"
-| "Turn off"
-| "Fade out"
-// | "Pause breathing"
-// | "Snake"
+	| "No animation"
+	| "Breathing"
+	| "Rainbow"
+	| "Strobe"
+	| "Turn off"
+	| "Fade out"
+	// | "Pause breathing"
+	// | "Snake"
+
+export interface BalancePidsProps {
+	pipUUID: PipUUID
+	targetAngle: number
+	pValue: number
+	iValue: number
+	dValue: number
+	ffValue: number
+	maxSafeAngleDeviation: number
+	updateInterval: number
+	deadbandAngle: number
+	maxStableRotation: number
+	minEffectivePwm: number
+}
