e52655455d9a271faa97d5b04afd509cabc9f0e0
Ariel Tamayev <actamayev@gmail.com>
2025-04-29 13:38:34 -0400

    Force package publish

--- src/types/public/api.ts
+++ src/types/public/api.ts
@@ -92,3 +92,4 @@ export type ErrorResponse = { error: string }
 export type ErrorResponses = ValidationErrorResponse | ErrorResponse
 export type NonSuccessResponse = MessageResponse | ErrorResponses
 export type AllCommonResponses = SuccessResponse | NonSuccessResponse
+
