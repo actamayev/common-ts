fc0d87b2beb0089d316f324fe5db7af71e89c8a4
Ariel Tamayev <actamayev@gmail.com>
2025-05-02 12:52:03 -0400

    Add start and end markers to the bytecode mesage

--- src/types/private/constants.ts
+++ src/types/private/constants.ts
@@ -4,3 +4,5 @@ export const MAX_PROGRAM_SIZE = 8192 // Maximum number of instructions
 export const MAX_REGISTERS = 512     // Maximum number of registers
 export const MAX_JUMP_DISTANCE = 65535 // Maximum jump distance in bytes (uint16_t max)
 export const INSTRUCTION_SIZE = 20   // Size of each instruction in bytes
+export const START_MARKER = 0xAA // 170 in decimal
+export const END_MARKER = 0x55   // 85 in decimal
