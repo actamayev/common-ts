1114f4ced67e173e912412e61d7e4fa3512a5d20
atamayev <bluedotrobots@gmail.com>
2025-08-03 11:46:17 -0400

    Update api.ts

--- src/types/public/api.ts
+++ src/types/public/api.ts
@@ -73,7 +73,7 @@ export interface CareerQuestChallengeData {
 export interface CareerProgressData {
 	currentChallengeUuidOrTextUuid: string
 	careerQuestChallengeData: CareerQuestChallengeData[]
-	isChallengeLocked: boolean
+	seenChallengeUUIDs: ChallengeUUID[]
 }
 
 // Chat:
