66219fc5560b7be8bcc178f3a6b81cad7c40323c
Ariel Tamayev <actamayev@gmail.com>
2025-09-10 14:23:43 -0400

    Update api.ts

--- src/types/api.ts
+++ src/types/api.ts
@@ -3,7 +3,7 @@ import { BlocklyJson, SandboxProject } from "./sandbox"
 import { StudentViewHubData, TeacherViewHubData } from "./hub"
 import { RetrievedQuestions, UserActivityProgress } from "./lab"
 import { ChallengeChatMessage, SandboxChatMessage } from "./chat"
-import { ChallengeUUID, ClassCode, HubUUID, PipUUID, SiteThemes } from "./utils"
+import { ChallengeUUID, ClassCode, HubUUID, SiteThemes } from "./utils"
 
 // Auth
 // Requests
@@ -124,6 +124,7 @@ export interface ProfilePictureUrl {
 export interface RetrieveIsPipUUIDValidResponse {
 	pipName: string | null
 	pipConnectionStatus: ESPConnectionStatus
+	userIdConnectedToPip: number | null
 }
 
 // Sandbox Routes
