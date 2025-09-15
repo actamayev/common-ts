1212c571dd07948103ba777f12bae3200d43893d
Ariel Tamayev <actamayev@gmail.com>
2025-09-15 13:25:06 -0400

    Simplify Pip Connection Status

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,4 +1,4 @@
-import { ESPConnectionStatus } from "./pip"
+import { PipConnectionStatus } from "./pip"
 import { BlocklyJson, SandboxProject } from "./sandbox"
 import { StudentViewHubData, TeacherViewHubData } from "./hub"
 import { RetrievedQuestions, UserActivityProgress } from "./lab"
@@ -123,7 +123,7 @@ export interface ProfilePictureUrl {
 
 export interface RetrieveIsPipUUIDValidResponse {
 	pipName: string | null
-	pipConnectionStatus: ESPConnectionStatus
+	pipConnectionStatus: PipConnectionStatus
 	isSomeoneConnectedToPip: boolean
 }
 
