cd3e4e582e61eb0178e385ea1ed47e82a57a1cf5
Ariel Tamayev <actamayev@gmail.com>
2025-10-18 16:00:24 -0400

    Update constants.ts

--- src/types/utils/constants.ts
+++ src/types/utils/constants.ts
@@ -7,8 +7,7 @@ export const INSTRUCTION_SIZE = 20   // Size of each instruction in bytes
 export const START_MARKER = 0xAA // 170 in decimal
 export const END_MARKER = 0x55   // 85 in decimal
 
-// Remove Z after Levi returns (PmKJZ)
 // Excludes easily confusable characters: 0, O, o, 1, l, I, S, 5, Z, 2, B, 8, G, 6, c, U, V, W, X, Y, u, v, w, x, y, n, N
-export const ACCEPTABLE_PIP_ID_CHARACTERS = "ACDEFGHJKLMNPQRTZadefhjkmpqrt3479"
+export const ACCEPTABLE_PIP_ID_CHARACTERS = "ACDEFGHJKLMNPQRTadefhjkmpqrt3479"
 // Excludes confusing characters: l (1), o (O), 0 (zero), and 1 (one)
 export const ACCEPTABLE_CLASS_CODE_CHARACTERS = "abcdefghijkmnpqrstuvwxyz23456789"
