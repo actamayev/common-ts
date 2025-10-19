a0cafb8c83c6a1efb10600c46fdf7587e9fb0537
lever-labs <hello@leverlabs.com>
2025-10-19 14:58:00 -0400

    Update constants.ts

--- src/types/utils/constants.ts
+++ src/types/utils/constants.ts
@@ -7,7 +7,8 @@ export const INSTRUCTION_SIZE = 20   // Size of each instruction in bytes
 export const START_MARKER = 0xAA // 170 in decimal
 export const END_MARKER = 0x55   // 85 in decimal
 
-// Excludes easily confusable characters: 0/O/o/c, 1/l/I, s/S/5, z/Z/2, B/8, 6/b, U/V, W/w, X/x, u/v, N (similar to H), G (similar to C)
-export const ACCEPTABLE_PIP_ID_CHARACTERS = "ACDEFHJKLMPQRTYadefghijkmnpqrty3479"
+// Excludes easily confusable characters:
+// 0/O/o/c, 1/l/I, s/S/5, z/Z/2, B/8, 6/b, U/V, W/w, X/x, u/v, K/k, N (similar to H), G (similar to C)
+export const ACCEPTABLE_PIP_ID_CHARACTERS = "ACDEFHJLMPQRTYadefghijmnpqrty3479"
 // Excludes confusing characters: l (1), o (O), 0 (zero), and 1 (one)
 export const ACCEPTABLE_CLASS_CODE_CHARACTERS = "abcdefghijkmnpqrstuvwxyz23456789"
