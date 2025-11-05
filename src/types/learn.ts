aa32286ea9c5bbfa723b3d43b95e521f77ad5b74
Ariel Tamayev <actamayev@gmail.com>
2025-11-05 17:34:13 -0500

    Update learn.ts

--- src/types/learn.ts
+++ src/types/learn.ts
@@ -79,7 +79,6 @@ export interface FunctionToBlockAnswerChoice {
 
 export interface MatchingAnswerChoice {
 	matchingAnswerChoicePairId: number
-	order: number
 	codingBlock: CodingBlock
 	matchingAnswerChoiceText: MatchingAnswerChoiceText
 }
